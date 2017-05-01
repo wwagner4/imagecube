0432f4c360f23d4193eba51190a8da0863eec788
wwagner4 <wwagner4@gmail.com>
2017-05-01 09:00:16 +0200

    moved code from test to implement

--- src/main/scala/imagecube1/Imagecube.scala
+++ src/main/scala/imagecube1/Imagecube.scala
@@ -1,7 +1,40 @@
 package imagecube1
 
-class Imagecube {
+case class Range(from: Int, to: Int)
 
+case class CutParams(x1: Range, x2: Range, x3: Range, y1: Range, y2: Range, y3: Range)
 
+object Imagecube {
+
+  def cutParams(width: Int, height: Int): CutParams = {
+
+    def cutParamsPort(w: Int, h: Int): CutParams = {
+      val h1 = (h / 3) * 3
+      val off1 = (w - h1) / 2
+      val step = h1 / 3
+      CutParams(
+        Range(off1, off1 + step - 1),
+        Range(off1 + step, off1 + (2 * step) - 1),
+        Range(off1 + (2 * step), off1 + (3 * step) - 1),
+        Range(0, step - 1),
+        Range(step, (2 * step) - 1),
+        Range(2 * step, (3 * step) - 1)
+      )
+    }
+
+    def transpose(p: CutParams): CutParams = {
+      CutParams(
+        p.y1,
+        p.y2,
+        p.y3,
+        p.x1,
+        p.x2,
+        p.x3
+      )
+    }
+
+    if (width >= height) cutParamsPort(width, height)
+    else transpose(cutParams(height, width))
+  }
 
 }
