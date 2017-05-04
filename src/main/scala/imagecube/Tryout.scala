6acb6d5d03690c559d277f023a823c1185c32dc0
wwagner4 <wwagner4@gmail.com>
2017-05-04 17:11:20 +0200

    removed unnecessary code and resources

--- src/main/scala/imagecube/Tryout.scala
+++ src/main/scala/imagecube/Tryout.scala
@@ -2,76 +2,298 @@ package imagecube
 
 import java.awt.Color
 import java.io.File
-import java.util.UUID
+
+import Imagecube._
 
 
 object Tryout extends App {
 
-  // convertColorToPixel()
-  // readImageToBufferedImage()
-  // exploreImage()
-  // copyImage()
-  cropImage()
-  // intArithm()
+  // readFile()
+  // createCutParams()
+  // transform()
+  // runFibs()
+  // diffs()
+  // compress()
+  // mean()
+  // rgb()
+  // shorten()
+  // shorten1()
+  // positionParts()
+  // parallel()
+  // perc()
+  // runExtractName()
+  runTestImages()
+  // runDir()
 
-  def intArithm(): Unit = {
-    val w = 7
-    val h = 3
-    val off1 = (w - h)  / 2
-    val off2 = w - off1
-    println(off1)
-    println(off2)
+  def runDir(): Unit = {
+
+
+    val inDir = new File("/Users/wwagner4/tmp/cubes/in")
+    val outDir = new File("/Users/wwagner4/tmp/cubes/out")
+    outDir.mkdirs()
+
+    val files = inDir.listFiles()
+
+
+    files.foreach{ f =>
+      writeImage(f, outDir)
+    }
+    println("FINISHED runDir")
   }
 
-  def dir: File = new File("src/main/resources")
+  def runTestImages(): Unit = {
+
+    val namesAll = List(
+      "cow",
+      "cow1",
+      "small",
+      "small1"
+    )
+
+    val startTime = System.currentTimeMillis()
+    namesAll.foreach{ name =>
+      val fName = s"$name.jpg"
+      val f = new File(dir, fName)
+      writeImage(f, tmpdir)
+    }
+    val stopTime = System.currentTimeMillis()
+    println(s"time: ${stopTime - startTime}")
+  }
+
+  def runExtractName(): Unit = {
+    Seq(
+      new File("a/b/c.png"),
+      new File("a/b/c"),
+      new File("a/b/cksjdfhskd_kasdjsal.png"),
+      new File("a/b/c.jpg.k"),
+    ).foreach { f => 
+      val name = extractName(f)
+      println(s"$f -> $name")
+    }
+  }
+
+  def perc(): Unit = {
+    val value = 1000
+    (1 to 20).foreach { p =>
+      val value1 = percent(value, p)
+      println(s"$value $p% -> $value1")
+    }
+  }
+
+  def positionParts(): Unit = {
+
+    val partLen = 2
+    val border = 1
+
+    val s = imageSize(partLen, border)
+    val pp = partPositions(partLen, border)
+
+    println(s)
+    println(pp)
+  }
+
+  def shorten1(): Unit = {
+
+    val fName = "cow.jpg"
+    val f = new File(dir, fName)
+    val img = readImage(f)
+
+    println(s"created img for $fName")
+
+    val shortImg = shortenImgPar(img)
+    println(s"shorted image from $fName - partLen: ${shortImg.partLen}")
+  }
+
+  def shorten(): Unit = {
+
+    val n = 10
+
+    val is = 0 to n
+    is.foreach { i =>
+      val (fa, ta) = shortenA(i, n)
+      val (fb, tb) = shortenB(i, n)
+      val la = ta - fa
+      val lb = tb - fb
+      println(f"shorten   $n%3d $i%3d : $fa%3d - $ta%3d ($la%3d) : $fb%3d - $tb%3d ($lb%3d)")
+    }
+  }
+
+  def rgb(): Unit = {
+
+    val colors = List(
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.BLUE,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED,
+      Color.RED
+    )
+
+    val ints = colors.map(_.getRGB)
+
+    val re = colorMix(ints)
+
+    println(re)
+    val c = new Color(re)
+    println(c)
+    println(Color.RED.getRGB)
+
+  }
+
+  def mean(): Unit = {
+
+    def m(a: Double, b: Double) = (a + b) / 2
+
+    val y1 = m(m(1, 2), 3)
+    val y2 = m(m(1, 3), 2)
+    val y3 = m(m(3, 2), 1)
+
+    println(y1)
+    println(y2)
+    println(y3)
+  }
+
+  def transform(): Unit = {
+
+    def diff(s: Stream[Int]): Stream[Int] = {
+      s.zip(s.tail).map { case (a, b) => b - a }
+    }
+
+    val n = 10
+    val ms = n to(3 * n, 1)
+
+    ms.foreach { m =>
+      val v = m.toDouble / n
+      val borders = Stream.iterate(0.0)(x => x + v).map(x => x.round.toInt)
+      println(f"$n%5d, $m%5d, $v%5.6f")
+      println("   " + borders.map(format).take(n + 1).mkString(", "))
+      println("     " + diff(borders).map(format).take(n).mkString(", "))
+      println
+    }
+
+    def format(v: Int): String = f"$v%3d"
+
+  }
+
+  def compress(): Unit = {
+    def mean(in: Seq[Double]): Double = in.sum / in.size
+    val in = List(1, 2, 3, 4, 2, 22, 23, 3, 24, 25, 72, 2, 2, 2, 2, 2, 2, 4, 2).map(_.toDouble)
+    (1 to in.size).foreach(n => println(linearCompress(in, n, mean).mkString(", ")))
+  }
+
+  lazy val fibs: Stream[Int] = 0 #:: 1 #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+
+
+  def diffs(): Unit = {
+    def diff(s: Stream[Int]): Stream[Int] =
+      s.zip(s.tail).map { case (a, b) => b - a }
+
+    println("FIBS: " + fibs.take(10).mkString(", "))
+    println("DIFFS: " + diff(fibs).take(10).mkString(", "))
+  }
+
+  def runFibs(): Unit = {
+    println(fibs.take(30).mkString(", "))
+  }
+
+
+  def createCutParams(): Unit = {
+    val whs = Seq(
+      (5, 6),
+      (5, 7),
+      (5, 8),
+      (6, 6),
+      (7, 6),
+      (8, 6)
+    )
+
+    whs.foreach {
+      case (w, h) => println(s"($w, $h, ${cutParams(w, h)}, (Range(0, 0), Range(0, 0))),")
+    }
+  }
+
+   def parallel(): Unit = {
+    val ran = new java.util.Random()
+        
+    def job(id: String, steps: Int): Int = {
+      println(f"JOB_$id STARTED")
+      (1 to steps).foreach { i => 
+        Thread.sleep(5 + ran.nextInt(95))
+        println(f"JOB_$id $i%3d/$steps%3d")
+      }
+      println(f"JOB_$id READY")
+      steps
+    }
+    
+    import scala.concurrent._
+    import ExecutionContext.Implicits.global
+    import scala.concurrent.duration._
+
+    val f1 = Future { job("A", 50)}
+    val f2 = Future { job("B", 10)}
+    val r: Future[(Int, Int)] = for(r1 <- f1; r2 <- f2) yield (r1, r2)
+    val (a, b): (Int, Int) = Await.result(r, Duration(10, SECONDS))
+    println(s"ready $a $b")  
+  }
+  
+ def readFile(): Unit = {
+    val fName = "tiny1.jpg"
+    val f = new File(dir, fName)
+    val img = readImage(f).toString.take(100) + " ..."
+    println(s"created img for $fName")
+    println(s"img: $img")
+  }
+
+
+  def dir: File = new File("src/test/resources")
+
   def tmpdir: File = {
     val re = new File("target/tmp")
     if (!re.exists()) re.mkdirs()
     re
-  }
-
-  def copyImage(): Unit ={
-    import imagecube.Imagecube._
-    println("reading image ...")
-    val img = readImage(new File(dir, "big1.jpg"))
-    val outName = s"out${UUID.randomUUID()}.jpg"
-    val outFile = new File(tmpdir, outName)
-    println("writing image ...")
-    writeImage(img, outFile)
-    println(s"wrote to $outFile")
-  }
-
-  def cropImage(): Unit ={
-    import imagecube.Imagecube._
-    println("reading image ...")
-    val img = readImage(new File(dir, "big1.jpg"))
-    println("cropping image ...")
-    val crop = cropSquare(img)
-    val outName = s"out${UUID.randomUUID()}.jpg"
-    val outFile = new File(tmpdir, outName)
-    println("writing image ...")
-    writeImage(crop, outFile)
-    println(s"wrote to $outFile")
-  }
-
-  def readImageToBufferedImage(): Unit = {
-    import Imagecube._
-    val img = readImage(new File(dir, "tiny.jpg"))
-    println(img)
-  }
-
-  def exploreImage(): Unit = {
-    import Imagecube._
-    val img = readImage(new File(dir, "tiny.jpg"))
-    println(img)
-  }
-
-  def convertColorToPixel(): Unit = {
-    val color = new Color(255, 20, 30)
-
-    println(color.getRed.getClass)
-    println(color.getGreen)
-    println(color.getBlue)
   }
 
 
--- src/test/scala/imagecube/ImagecubeSuite.scala
+++ src/test/scala/imagecube/ImagecubeSuite.scala
@@ -1,56 +1,48 @@
 package imagecube
 
-import java.io.File
-
-import org.scalatest._
+import org.scalatest.FunSuite
+import Imagecube._
 
 class ImagecubeSuite extends FunSuite {
 
-  import Imagecube._
-
-
-  def image23: Image = {
-    val px = Seq(1, 2, 3, 4, 5, 6)
-    Image(2, 3, px)
-  }
-
-  test("pixel") {
-    val p = pixel(image23)
-    assert(p.size === 6)
-    assert(p(0) === Pixel(0, 0, 1))
-    assert(p(1) === Pixel(1, 0, 2))
-    assert(p(2) === Pixel(0, 1, 3))
-    assert(p(3) === Pixel(1, 1, 4))
-    assert(p(4) === Pixel(0, 2, 5))
-    assert(p(5) === Pixel(1, 2, 6))
-  }
-
-  Seq(
-    (0, Seq(1, 2)),
-    (1, Seq(3, 4)),
-    (2, Seq(5, 6))
-  ).foreach { case (i, should) =>
-    test(s"rows$i") {
-      assert(rows(image23)(i) === should)
-    }
-  }
-
-  val imagTypeTestdata = List(
-    ("PNG", "a.png"),
-    ("PNG", "x/b.png"),
-    ("PNG", "all.Png"),
-    ("PNG", "a/a.PNG"),
-    ("JPG", "a/a.jpg"),
-    ("JPG", "a/a.jpeg"),
-    ("JPG", "a/a.JPEG"),
-    ("JPG", "a/a.JPG")
+  val cutParamsTestValues = List(
+    (7, 6, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(0, 1), Range(2, 3), Range(4, 5))),
+    (9, 7, CutParams(2, Range(1, 2), Range(3, 4), Range(5, 6), Range(0, 1), Range(2, 3), Range(4, 5))),
+    (6, 10, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(2, 3), Range(4, 5), Range(6, 7))),
+    (12, 10, CutParams(3, Range(1, 3), Range(4, 6), Range(7, 9), Range(0, 2), Range(3, 5), Range(6, 8)))
   )
-  imagTypeTestdata.foreach { case (should, is) =>
-    test(s"imageType$should$is") {
-      assert(imageType(new File(is)) === should)
-    }
 
+  cutParamsTestValues.foreach {
+    case (w, h, p) => test(f"cutParams-$w%03d-$h%03d") {
+      assert(cutParams(w, h) === p)
+    }
   }
 
+  val transposeParamsLeftTestValues = Seq(
+    (5, 6, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(1, 1), Range(2, 2), Range(3, 3)), (Range(3, 1), Range(0, 0))),
+    (5, 7, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(2, 2), Range(3, 3), Range(4, 4)), (Range(4, 2), Range(0, 0))),
+    (5, 8, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(2, 2), Range(3, 3), Range(4, 4)), (Range(4, 2), Range(0, 0))),
+    (6, 6, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(0, 1))),
+    (7, 6, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(0, 1))),
+    (8, 6, CutParams(2, Range(1, 2), Range(3, 4), Range(5, 6), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(1, 2)))
+  )
+  transposeParamsLeftTestValues.foreach {
+    case (w, h, p, should) => test(f"transposeParamsLeftTestValues-$w%03d-$h%03d") {
+      assert(transposeParamsLeft(p) === should)
+    }
+  }
 
+  val transposeParamsRightTestValues = Seq(
+    (5, 6, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(1, 1), Range(2, 2), Range(3, 3)), (Range(3, 1), Range(2, 2))),
+    (5, 7, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(2, 2), Range(3, 3), Range(4, 4)), (Range(4, 2), Range(2, 2))),
+    (5, 8, CutParams(1, Range(0, 0), Range(1, 1), Range(2, 2), Range(2, 2), Range(3, 3), Range(4, 4)), (Range(4, 2), Range(2, 2))),
+    (6, 6, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(4, 5))),
+    (7, 6, CutParams(2, Range(0, 1), Range(2, 3), Range(4, 5), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(4, 5))),
+    (8, 6, CutParams(2, Range(1, 2), Range(3, 4), Range(5, 6), Range(0, 1), Range(2, 3), Range(4, 5)), (Range(5, 0), Range(5, 6)))
+  )
+  transposeParamsRightTestValues.foreach {
+    case (w, h, p, should) => test(f"transposeParamsRightTestValues-$w%03d-$h%03d") {
+      assert(transposeParamsRight(p) === should)
+    }
+  }
 }
