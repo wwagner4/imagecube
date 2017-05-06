99033ad0337fd4a369a97d3d3b03280a7afee869
wwagner4 <wwagner4@gmail.com>
2017-05-06 07:59:11 +0200

    web app somehow working

---
+++ web/src/main/scala/Templates.scala
@@ -0,0 +1,44 @@
+package web.imagecube
+
+sealed trait BGCOL { def txt: String }
+case object BGCOL_normal extends BGCOL {def txt = "#ffc299"}
+case object BGCOL_alarm extends BGCOL {def txt = "#ff8533"}
+
+object Templates {
+  
+ def templ(content: String, bg: BGCOL): String = {
+       s"""
+<html>
+<head>
+<title>imagecube</title>
+<style>
+body {
+    background-color: ${bg.txt};
+    font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+    font-size: 200%;
+    padding: 20px;
+}
+.b {
+    font-weight: bold;		
+}	
+.s {
+    font-size: 50%;		
+}	
+</style>
+</head>
+<body>
+<h1>imagecube</h1>
+$content
+<p class="s"><a href="http://entelijan.net">entelijan.net ... </a>
+</body>
+</html>  
+   """
+ }
+  
+def home: String = {
+  """
+        <p><a href="/">back to start ...</a></p>
+  """
+}
+  
+}
--- web/src/main/scala/web/imagecube/ImagecubeServlet.scala
+++ web/src/main/scala/web/imagecube/ImagecubeServlet.scala
@@ -3,6 +3,7 @@ package web.imagecube
 import org.scalatra._
 import servlet.{MultipartConfig, SizeConstraintExceededException, FileUploadSupport}
 import xml.Node
+import Templates._
 
 class ImagecubeServlet extends ScalatraServlet with FileUploadSupport with FlashMapSupport {
   
@@ -12,99 +13,57 @@ class ImagecubeServlet extends ScalatraServlet with FileUploadSupport with Flash
   
     error {
     case e: SizeConstraintExceededException =>
-      RequestEntityTooLarge(
-    <html>
-      <body>
-        <p>The file you uploaded exceeded the {limit} MB limit.</p>
-      </body>
-    </html>
-    )
+          contentType="text/html"
+        val content = s"""
+        <p class="b">The file you uploaded exceeded the ${limit} MB limit.</p>
+        $home
+        """
+        templ(content, BGCOL_alarm)
   }
 
   get("/") {
     contentType="text/html"
-    """
-<html>
-<head>
-<title>imagecube</title>
-<style>
-body {
-    background-color: #ffc299;
-    //background-color: #ff8533;
-    font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
-    font-size: 200%;
-    padding: 20px;
-}
-</style>
-</head>
-<body>
-<h1>imagecube</h1>
-<p>select an image in order to create your own imagecube</p>
-<p><a href="http://entelijan.net">entelijan.net</a>
-</body>
-</html>  
-   """
-  }
-  get("/ul") {
-    <html>
-      <body>
-        <h1>upload page</h1>
-      <form action={url("/upload")} method="post" enctype="multipart/form-data">
-       <p>File to upload: <input type="file" name="file" value = "select file"/></p>
-       <p><input type="submit" value="upload" /></p>
+    val content = s"""
+      <form action="/upload" method="post" enctype="multipart/form-data">
+       <p>File to be transformed <input type="file" name="file" value = "select file"/></p>
+       <p><input type="submit" value="transform" /></p>
       </form>
       <p>
-        Upload a file using the above form. After you hit "Upload"
-        the file will be uploaded and your browser will start
-        downloading it.
+        Select a file. After you hit "transform"
+        an imagecube will be created and downloaded to your computer
       </p>
 
-      <p>
-        The maximum file size accepted is {limit} MB.
-      </p>
-      </body>
-    </html>
+      <p> The maximum file size accepted is ${limit} MB</p>
+    """
+    templ(content, BGCOL_normal)
   }
-    post("/upload") {
+  post("/upload") {
     fileParams.get("file") match {
       case Some(file) =>
         println(s"found a file - $file - size:${file.getSize} - type:${file.contentType.getOrElse("???")}")
         if (file.getSize == 0) {
-          BadRequest(
-      <html>
-        <body>
-            <p>
-              Hey! You forgot to select a file.
-            </p>
-          <p>
-            <a href="/">home</a>.
-          </p>
-        </body>
-      </html>
-            )
+          contentType="text/html"
+          val content = s"""
+          <p  class="b">Hey! You forgot to select a file.</p>
+          $home
+          """
+          templ(content, BGCOL_alarm)
         } else {
-        Ok(file.get(), Map(
-          "Content-Type"        -> (file.contentType.getOrElse("application/octet-stream")),
-          "Content-Disposition" -> ("attachment; filename=\"" + file.name + "\"")
-        ))
+          Ok(file.get(), Map(
+            "Content-Type"        -> (file.contentType.getOrElse("application/octet-stream")),
+            "Content-Disposition" -> ("attachment; filename=\"" + file.name + "\"")
+          ))
         }
-        
-
       case None =>
-        println("bad request - NO FILE")
-        BadRequest(
-    <html>
-      <body>
-          <p>
-            Hey! You forgot to select a file.
-          </p>
-          <p>
-            <a href="/">home</a>.
-          </p>
-      </body>
-    </html>
-          )
+        contentType="text/html"
+        val content = s"""
+        <p  class="b">Hey! You forgot to select a file.</p>
+        $home
+        """
+        templ(content, BGCOL_alarm)
     }
   }
+  
+
 
 }
