05d9c92262da4725796b31327de87d6f79e61b6e
wwagner4 <wwagner4@gmail.com>
2017-05-11 00:14:05 +0200

    refactoring

--- web/src/main/scala/web/imagecube/JettyLauncher.scala
+++ web/src/main/scala/web/imagecube/JettyLauncher.scala
@@ -1,7 +1,6 @@
 package web.imagecube
 
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
@@ -18,7 +17,7 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
 
     server.setHandler(context)
 
-    server.start
-    server.join
+    server.start()
+    server.join()
   }
 }
